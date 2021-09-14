adc52498b3354ed20b59f39244222c761146ef5d
Derian <dpinto@ecuavisa.com>
2021-09-14 10:19:20 -0500

    Update login

--- login.php
+++ login.php
@@ -1,25 +1,57 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-$code = "KO";
+include "Curl.php";
+include "Helper.php";
+
+$publicKey = "86f1d5db-6fdf-41f8-8206-267d88f8b57c";
+$privateKey = 'uH9AwqsWKKYPUalWuShcQA==';
+
+$data = [];
+$data["msg"] = "Error login";
+$data["status"] = "KO";
 
 if (!empty($_POST))
 {
-    if(isset($_POST["accesstoken"])){
-        $code = "OK";
-        $msg = '<field name="output">
-        <string>'.$_POST["accesstoken"].'</string>
-        </field>';
+    if(isset($_POST["user_id"])){
+        $userId = $_POST["user_id"];
+        $api = Curl::GetPage([
+		    "url" => "https://suscripciones.ecuavisa.com/api/v1/users/".$userId,
+		    "requestHeaders" => [
+		        "Authorization" => "Bearer wyleex+vistazo" 
+		    ]
+		]);
+		$apiData = json_decode($api);
+		$apiData = $apiData->data;
+		$pos = strpos($apiData->email, "@facebook.com");
+		if($pos === false){
+		    $username = explode("@", $apiData->email);
+		    $username = $username[0];
+
+		    $userEncription = [
+		        "usrname" => $username,
+		        "first_name"  => $apiData->first_name,
+		        "email" => $apiData->email,
+		        "pwdmd5" => (String) md5($apiData->email),
+		        'extid' => 'WYECU'.$apiData->id,
+		        "timestamp" => (String) strtotime($apiData->created_at)
+		    ];
+		    
+		    $text = json_encode($userEncription);
+		    $encrypted = encrypt($text, $publicKey, $privateKey);
+		    $url = urlencode("https:/www.ecuavisa.com");
+		    $codificado = "1,". $publicKey. "," .$encrypted;
+		    $urlProtect = "https://www.ecuavisa.com/user-portlet/refreshuserentitlements?redirect=".$url."&ssodata=".urlencode($codificado);
+		    $apiProtec = Curl::GetPage([
+		        "url" => $urlProtect,
+		    ]);
+		    $data["msg"] = "User: ".$username." has accessed.";
+			$data["status"] = "OK";
+		}
     }
-}else{
-    $msg =  '<field name="msg">
-    <string>User identity could not be verified. Please, try it again</string>
-    </field>';
 }
-header ("Content-Type:text/xml");
-echo '<?xml version="1.0"?>
-<itwresponse version="1.0">
-<field name="code">
-<string>'.$code.'</string>
-</field>
-'.$msg.'
-</itwresponse>';
+
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($data);
